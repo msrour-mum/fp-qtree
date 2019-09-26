4611c74b2d4573bcdf638268d2711166531c6871
Emad <eshehate@mum.edu>
2019-09-26 07:44:53 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/test/Java/App/AnalysisFunctionsTest.java
@@ -0,0 +1,21 @@
+package App;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnalysisFunctionsTest {
+
+    @Test
+    void getKViewedQuestions() {
+    }
+
+    @Test
+    void getKTrendingTags() {
+
+    }
+
+    @Test
+    void getKFakeAnswers() {
+    }
+}
